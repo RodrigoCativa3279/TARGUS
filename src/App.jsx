93142214ecc7e220c2a1dc3d4b953ab3c3d78573
Cativa Rodrigo <cativa.rodrigoet36@gmail.com>
2025-06-11 22:02:21 -0300

    Agregar imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import './App.css';
 
+import tituloImg from './assets/Titulo.png';
+import iconCuenta from './assets/iconcuenta.png';
+import juego1 from './assets/Juego 1.png';
+import juego2 from './assets/Juego 2.png';
+import juego3 from './assets/Juego 3.png';
+import juego4 from './assets/Juego 4.png';
+import juego5 from './assets/Juego 5.png';
+import juego6 from './assets/Juego 6.png';
+
 function App() {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light">
         <div className="d-flex align-items-center col-10">
-          <img src="/img/Titulo.png" className="logo img-fluid" alt="Titulo" />
+          <img src={tituloImg} className="logo img-fluid" alt="Titulo" />
         </div>
         <button
           className="navbar-toggler col-2"
@@ -23,7 +32,7 @@ function App() {
           <ul className="navbar-nav mr-auto">
             <li className="nav-item active row">
               <a className="nav-link" href="paginaconfig.html">
-                <img src="/img/iconcuenta.png" className="iconcuenta rounded-circle img-fluid" alt="Cuenta" />
+                <img src={iconCuenta} className="iconcuenta rounded-circle img-fluid" alt="Cuenta" />
                 <span className="sr-only">(current)</span>
               </a>
             </li>
@@ -36,17 +45,17 @@ function App() {
           <h1 className="categoria">Clasicos</h1>
           <div className="grid-juegos">
             <a href="#" className="juego enlace-juego">
-              <img src="/img/Juego 1.png" alt="Juego 1" />
+              <img src={juego1} alt="Juego 1" />
               <h4 className="tituloJuego">Crucigrama</h4>
               <p className="descripcion">¡Desafía tu mente y divertite completando el crucigrama!</p>
             </a>
             <a href="#" className="juego enlace-juego">
-              <img src="/img/Juego 2.png" alt="Juego 2" />
+              <img src={juego2} alt="Juego 2" />
               <h4 className="tituloJuego">Sudoku</h4>
               <p className="descripcion">¡Entrená tu lógica con Sudoku y resolvé nuevos desafíos diarios!</p>
             </a>
             <a href="#" className="juego enlace-juego">
-              <img src="/img/Juego 3.png" alt="Juego 3" />
+              <img src={juego3} alt="Juego 3" />
               <h4 className="tituloJuego">Sopa de letras</h4>
               <p className="descripcion">¡Encontrá todas las palabras ocultas y poné a prueba tu agudeza visual!</p>
             </a>
@@ -57,17 +66,17 @@ function App() {
           <h1 className="categoria">Nuevos</h1>
           <div className="grid-juegos">
             <a href="#" className="juego enlace-juego">
-              <img src="/img/Juego 4.png" alt="Juego 4" />
+              <img src={juego4} alt="Juego 4" />
               <h4 className="tituloJuego">Adivina la palabra</h4>
               <p className="descripcion">Gorras</p>
             </a>
             <a href="#" className="juego enlace-juego">
-              <img src="/img/Juego 5.png" alt="Juego 5" />
+              <img src={juego5} alt="Juego 5" />
               <h4 className="tituloJuego">¿Mayor o menor?</h4>
               <p className="descripcion">Botines</p>
             </a>
             <a href="#" className="juego enlace-juego">
-              <img src="/img/Juego 6.png" alt="Juego 6" />
+              <img src={juego6} alt="Juego 6" />
               <h4 className="tituloJuego">Adivina el país</h4>
               <p className="descripcion">Camperas deportivas</p>
             </a>
